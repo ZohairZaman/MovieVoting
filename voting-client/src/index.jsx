32089a64076d80bcdc5a48cae7e8b14c7a04a04f
Zohair Zaman <zohairzaman@CHIC02T84N8H03Q.grubhub.local>
2017-06-18 21:17:47 -0500

    Full project push - fully functional

---
+++ voting-client/src/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory} from 'react-router';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducer';
+import App from './components/App';
+import {VotingContainer} from './components/Voting';
+import {ResultsContainer} from './components/Results';
+import io from 'socket.io-client';
+import {setState} from './action_creators';
+import remoteActionMiddleware from './remote_action_middleware';
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('state', state =>
+  store.dispatch(setState(state))
+);
+
+const createStoreWithMiddleware = applyMiddleware(
+  remoteActionMiddleware(socket)
+)(createStore);
+const store = createStoreWithMiddleware(reducer);
+
+const routes = <Route component={App}>
+  <Route path="/results" component={ResultsContainer} />
+  <Route path="/" component={VotingContainer}/>
+</Route>;
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={hashHistory}>{routes}</Router>
+  </Provider>,
+  document.getElementById('app')
+);
---
+++ voting-server/test/immutable_spec.js
@@ -0,0 +1,22 @@
+import {expect} from 'chai';
+import {List} from 'immutable';
+
+describe('immutability', () => {
+
+  describe('a number', () => {
+
+    function increment(currentState) {
+      return currentState+1;
+    }
+
+    it('is immutable', () => {
+      let state = 42;
+      let nextState = increment(state);
+
+      expect(nextState).to.equal(43);
+      expect(state).to.equal(42);
+    });
+
+  });
+
+});
